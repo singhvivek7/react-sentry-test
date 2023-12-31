fe7b1fe2f2d432fb61360d2d30d88dc7d3bc8591
Vivek Kumar <mail.help.vivek@gmail.com>
2023-12-31 17:14:18 +0530

    sentry implemented

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://146c86146fcdd99ff7bb9f4abb626290@o4506409620471808.ingest.sentry.io/4506490114801664",
+  integrations: [
+    new Sentry.BrowserTracing({
+      // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+      tracePropagationTargets: [import.meta.env?.VITE_APP_BASE_URL ?? ""],
+    }),
+    new Sentry.Replay({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { sentryVitePlugin } from "@sentry/vite-plugin";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    sentryVitePlugin({
+      org: "vivek-inc",
+      project: "react-test",
+      authToken: process.env?.VITE_APP_SENTRY_AUTH_TOKEN ?? "",
+    }),
+  ],
+
+  build: {
+    sourcemap: true,
+  },
+});
